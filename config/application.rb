a60993d08db9e52994a0d2900cf1ba3e5bf4e955
Leo Royzengurt <rebellabs@gmail.com>
2014-05-26 23:03:45 -0400

    remove precompile

--- config/application.rb
+++ config/application.rb
@@ -49,7 +49,6 @@ module MettleMe
     # config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
